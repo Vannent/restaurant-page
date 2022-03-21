7431ad86e03316d6781bbfcaf62c0543f3f65c9f
Vannent <erobced@gmail.com>
2022-03-21 17:21:15 -0500

    add imgs and pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Testiana");
+// import "./styles/home.css";
+// import "./styles/menu.css";
+// import "./styles/about.css";
+// import home from "./pages/home";
+// import menu from "./pages/menu";
+// import contact from "./pages/contact";
+
+import header from "./pages/navegation";
+
+header();
---
+++ src/pages/navegation.js
@@ -0,0 +1,7 @@
+import homeIcon from "./img/home.png";
+import menuIcon from "./img/menu.png";
+import contactIcon from "./img/contact.png";
+import home from "./home";
+import menu from "./menu";
+import contact from "./contact";
+
