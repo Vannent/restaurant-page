21608b87bee11d53f90cac44d304a32a273a20d8
Vannent <erobced@gmail.com>
2022-03-22 16:53:12 -0500

    add banner content

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,27 @@
+import loadMenu from "./menu";
+
 const home = () => {
     const banner = document.createElement("div");
     banner.className = "banner";
+
+    const bannerTitle = document.createElement("h1");
+    bannerTitle.className = "banner-title"
+    bannerTitle.textContent = "Delicious Food";
+
+    const bannerParagraph = document.createElement("p");
+    bannerParagraph.className = "banner-paragraph"
+    bannerParagraph.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+    const checkMenu = document.createElement("button")
+    checkMenu.className = "checkMenu";
+    checkMenu.textContent = "Check our Menu!"
+    checkMenu.addEventListener("click", (e) => {
+        loadMenu();
+      });
+
+    banner.appendChild(bannerTitle);
+    banner.appendChild(bannerParagraph);
+    banner.appendChild(checkMenu);
 
     return banner;
 };
